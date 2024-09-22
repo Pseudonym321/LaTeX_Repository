2835a2e2031f918c4dc1bf6656c7bb17ff8243f8
Jasper <twilliam1925@outlook.com>
2024-09-22 12:46:36 -0600

    projections_of_spinning_sphere

--- TikZ/code_for_animations/loxodromic_toroidal_spiral.py
+++ TikZ/code_for_animations/loxodromic_toroidal_spiral.py
@@ -1,8 +1,7 @@
-
 import numpy as np
 import Animation_Modules.animatetex as animatetex
 
-numiter = 24
+numiter = 180
 
 preamble= r'''
 \documentclass{beamer}
@@ -18,7 +17,7 @@ postscript = r'''
 \begin{tikzpicture}[tdplot_main_coords]
 \draw[white,tdplot_main_coords] (-4.5,-3.5) rectangle (4.5,5);
 \tdplotsetrotatedcoords{0}{0}{0}
-\draw[tdplot_rotated_coords,domain=90:\Vn,smooth,very thin,samples=5000,variable=\Vt] plot ({sqrt(1-(\Vt/360)^2)*sin(40*\Vt)/(1-sqrt(1-(\Vt/360)^2)*cos(40*\Vt)))},{(\Vt/360)/(1-sqrt(1-(\Vt/360)^2)*cos(40*\Vt))*cos(3*\Vt)},{(\Vt/360)/(1-sqrt(1-(\Vt/360)^2)*cos(40*\Vt))*sin(3*\Vt)});
+\draw[tdplot_rotated_coords,domain=\Vn:360,smooth,very thin,samples=1000,variable=\Vt] plot ({1*sqrt(1-(\Vt/360)^2)*sin(40*\Vt)/(1-sqrt(1-(\Vt/360)^2)*cos(40*\Vt)))},{1*(\Vt/360)/(1-sqrt(1-(\Vt/360)^2)*cos(40*\Vt))*cos(10*\Vt)},{1*(\Vt/360)/(1-sqrt(1-(\Vt/360)^2)*cos(40*\Vt))*sin(10*\Vt)});
 \end{tikzpicture}
 \end{document}
 '''
@@ -26,14 +25,14 @@ postscript = r'''
 def main():
     """
     Purpose:
-        Makes a spinning loxodrome projection.
+        Makes a stereographic projection of a loxodrome that follows the trajectory of a nested torus. 
     Parameters:
         No parameters.
     Return:
         Void.
     """
     animatetex.before_loop()
-    for n in np.linspace(90,360,numiter):
+    for n in np.linspace(360,300,numiter):
         with open(animatetex.TeX_file, 'w') as TeX:
             TeX.write(preamble)
             TeX.write(r'\newcommand{\Vn}{' +f'{n}' +'}')
