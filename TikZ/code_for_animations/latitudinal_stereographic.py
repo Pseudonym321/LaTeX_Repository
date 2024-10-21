873390b31fc0fa8724261dae0ffc8e207712b092
Jasper <twilliam1925@outlook.com>
2024-10-21 13:18:38 -0600

    Working on improving readability + editing syntax to be more up-to-snuff

--- TikZ/code_for_animations/latitudinal_stereographic.py
+++ TikZ/code_for_animations/latitudinal_stereographic.py
@@ -6,52 +6,49 @@ numiter = 24
 start= r'''
 \documentclass{beamer}
 \beamertemplatenavigationsymbolsempty
-%tikzpicture
+
 \usepackage{tikz}
 \usetikzlibrary{arrows.meta}
-
 '''
+
 end = r'''
-
-
-\newcommand{\myscale}{2.1}
 \begin{document}
-\begin{frame}
-\begin{tikzpicture}[scale=\myscale]
-%%% GEOMETRY %%%
-\draw[] (0,0) circle [radius=1];
-\draw[] (0,0) -- (\mytheta:1);
+    \begin{frame}
+        \centering
+        \begin{tikzpicture}
+            %%% GEOMETRY %%%
+            \draw[] (0,0) circle [radius=1];
+            \draw[] (0,0) -- (\varT:1);
 
-%%% LINES %%%
-\draw[] (0,1) -- ({cos(\mytheta)},{sin(\mytheta)});
-\draw[] (0,1) -- ({cos(\mytheta)/(1-sin(\mytheta))},0);
+            %%% LINES %%%
+            \draw[] (0,1) -- ({cos(\varT)},{sin(\varT)});
+            \draw[] (0,1) -- ({cos(\varT)/(1-sin(\varT))},0);
 
-\fill[] ({cos(\mytheta)/(1-sin(\mytheta))},0) circle [radius=0.05];
-\draw[] ({cos(\mytheta)},{sin(\mytheta)}) -- ({cos(\mytheta)},{-sin(\mytheta)});
-\fill[] ({cos(\mytheta)},{-sin(\mytheta)}) circle [radius=0.05];
+            \fill[] ({cos(\varT)/(1-sin(\varT))},0) circle [radius=0.05];
+            \draw[] ({cos(\varT)},{sin(\varT)}) -- ({cos(\varT)},{-sin(\varT)});
+            \fill[] ({cos(\varT)},{-sin(\varT)}) circle [radius=0.05];
 
-\draw[] (0,1) -- ({cos(\mytheta)},{-sin(\mytheta)});
-\draw[] (0,1) -- ({cos(\mytheta)/(1+sin(\mytheta))},0);
-\fill[] ({cos(\mytheta)/(1+sin(\mytheta))},0) circle [radius=0.05];
+            \draw[] (0,1) -- ({cos(\varT)},{-sin(\varT)});
+            \draw[] (0,1) -- ({cos(\varT)/(1+sin(\varT))},0);
+            \fill[] ({cos(\varT)/(1+sin(\varT))},0) circle [radius=0.05];
 
-\draw[thin,densely dotted] ({cos(\mytheta)/(1-sin(\mytheta))},0) -- ({cos(\mytheta)/(1-sin(\mytheta))},-1.3);
-\draw[thin,densely dotted] ({cos(\mytheta)/(1+sin(\mytheta))},0) -- ({cos(\mytheta)/(1+sin(\mytheta))},-1.3);
-\draw[|-|] ({cos(\mytheta)/(1-sin(\mytheta))},-1.3) -- ({cos(\mytheta)/(1+sin(\mytheta))},-1.3);
+            \draw[thin,densely dotted] ({cos(\varT)/(1-sin(\varT))},0) -- ({cos(\varT)/(1-sin(\varT))},-1.3);
+            \draw[thin,densely dotted] ({cos(\varT)/(1+sin(\varT))},0) -- ({cos(\varT)/(1+sin(\varT))},-1.3);
+            \draw[|-|] ({cos(\varT)/(1-sin(\varT))},-1.3) -- ({cos(\varT)/(1+sin(\varT))},-1.3);
 
+            %%% AXES %%%
+            \draw[-latex,thick] (0,0) -- (0,2);
+            \draw[-latex,thick] (-1.5,0) -- (3,0);
 
+            %%% NODES %%%
+            \fill[] (0,0) circle [radius=0.05];
+            \fill[] (0,1) circle [radius=0.05];
+            %\fill[] (\varT+180:1) circle [radius=0.05];
+            \fill[] (\varT:1) circle [radius=0.05];
+            \draw[red,|-Latex] ({1/cos(\varT)},-1.5) -- ({1/cos(\varT)},0);
 
-%%% AXES %%%
-\draw[-latex,thick] (0,0) -- (0,2);
-\draw[-latex,thick] (-1.5,0) -- (3,0);
-
-%%% NODES %%%
-\fill[] (0,0) circle [radius=0.05];
-\fill[] (0,1) circle [radius=0.05];
-%\fill[] (\mytheta+180:1) circle [radius=0.05];
-\fill[] (\mytheta:1) circle [radius=0.05];
-\draw[red,|-Latex] ({1/cos(\mytheta)},-1.5) -- ({1/cos(\mytheta)},0);
-\end{tikzpicture}
-\end{frame}
+        \end{tikzpicture}
+    \end{frame}
 \end{document}
 '''
 def main():
@@ -59,13 +56,13 @@ def main():
     for angle in np.linspace(0,50,numiter//2):
         with open(animatetex.TeX_file, 'w') as f:
             f.write(start)
-            f.write(r'\newcommand{\mytheta}{' +f'{angle}' +'}')
+            f.write(r'\newcommand{\varT}{' +f'{angle}' +'}')
             f.write(end)
         animatetex.during_loop()
     for angle in np.linspace(50,0,numiter//2):
         with open(animatetex.TeX_file, 'w') as f:
             f.write(start)
-            f.write(r'\newcommand{\mytheta}{' +f'{angle}' +'}')
+            f.write(r'\newcommand{\varT}{' +f'{angle}' +'}')
             f.write(end)
         animatetex.during_loop()
     animatetex.after_loop()
--- TikZ/code_for_animations/longitudinal_stereographic_projection.py
+++ TikZ/code_for_animations/longitudinal_stereographic_projection.py
@@ -5,57 +5,43 @@ numiter = 24
 start= r'''
 \documentclass{beamer}
 \beamertemplatenavigationsymbolsempty
-%maths
-\usepackage{mathtools}
-\usepackage{amsmath}
-\usepackage{amssymb}
-\usepackage{amsfonts}
-\usepackage{autobreak}
-\usepackage{comment}
+
 %tikzpicture
 \usepackage{tikz}
-\usepackage{scalerel}
-\usepackage{pict2e}
 \usepackage{tkz-euclide}
-\usepackage{tikz-3dplot}
-\usetikzlibrary{calc}
-\usetikzlibrary{patterns,arrows.meta}
-\usetikzlibrary{shadows}
-\usetikzlibrary{external}
-\usetikzlibrary{decorations.pathreplacing,angles,quotes}
-\usetikzlibrary{perspective,spath3}
-
+\usetikzlibrary{arrows.meta}
 '''
 end = r'''
-
-\newcommand{\myscale}{2.1}
 \begin{document}
-\begin{frame}
-\begin{tikzpicture}[scale=\myscale]
-%%% GEOMETRY %%%
-\draw[] (0,0) circle [radius=1];
-\draw[] (\mytheta+180:1) -- (\mytheta:1);
-\draw[] (\mytheta+180:1) -- (0,1);
-\fill[] ({(-cos(\mytheta))/(sin(\mytheta)+1)},0) circle [radius=0.05];
-\fill[] ({(cos(\mytheta))/(1-sin(\mytheta))},0) circle [radius=0.05];
-\draw[] (0,1) -- ({(cos(\mytheta))/(1-sin(\mytheta))},0);
-\draw[densely dotted,thin] ({(cos(\mytheta))/(1-sin(\mytheta))},0) -- ({(cos(\mytheta))/(1-sin(\mytheta))},-1.3);
-\draw[densely dotted,thin] ({(-cos(\mytheta))/(sin(\mytheta)+1)},0) -- ({(-cos(\mytheta))/(sin(\mytheta)+1)},-1.3);
-\draw[|-|] ({(-cos(\mytheta))/(sin(\mytheta)+1)},-1.3) -- ({(cos(\mytheta))/(1-sin(\mytheta))},-1.3);
-\draw[] (0,1) -- ({cos(\mytheta)},{sin(\mytheta)});
-\draw[] (0,1) -- ({-cos(\mytheta)/(1+sin(\mytheta)},0);
-%%% AXES %%%
-\draw[-latex,thick] (0,0) -- (0,2);
-\draw[-latex,thick] (-2,0) -- (3,0);
+    \begin{frame}
+        \centering
+        \begin{tikzpicture}
 
-%%% NODES %%%
-\fill[] (0,0) circle [radius=0.05];
-\fill[] (0,1) circle [radius=0.05];
-\fill[] (\mytheta+180:1) circle [radius=0.05];
-\fill[] (\mytheta:1) circle [radius=0.05];
-\draw[red,|-Latex] ({tan(\mytheta)},-1.5) -- ({tan(\mytheta)},0);
-\end{tikzpicture}
-\end{frame}
+            %%% GEOMETRY %%%
+            \draw[] (0,0) circle [radius=1];
+            \draw[] (\varT+180:1) -- (\varT:1);
+            \draw[] (\varT+180:1) -- (0,1);
+            \fill[] ({(-cos(\varT))/(sin(\varT)+1)},0) circle [radius=0.05];
+            \fill[] ({(cos(\varT))/(1-sin(\varT))},0) circle [radius=0.05];
+            \draw[] (0,1) -- ({(cos(\varT))/(1-sin(\varT))},0);
+            \draw[densely dotted,thin] ({(cos(\varT))/(1-sin(\varT))},0) -- ({(cos(\varT))/(1-sin(\varT))},-1.3);
+            \draw[densely dotted,thin] ({(-cos(\varT))/(sin(\varT)+1)},0) -- ({(-cos(\varT))/(sin(\varT)+1)},-1.3);
+            \draw[|-|] ({(-cos(\varT))/(sin(\varT)+1)},-1.3) -- ({(cos(\varT))/(1-sin(\varT))},-1.3);
+            \draw[] (0,1) -- ({cos(\varT)},{sin(\varT)});
+            \draw[] (0,1) -- ({-cos(\varT)/(1+sin(\varT)},0);
+            
+            %%% AXES %%%
+            \draw[-latex,thick] (0,0) -- (0,2);
+            \draw[-latex,thick] (-2,0) -- (3,0);
+
+            %%% NODES %%%
+            \fill[] (0,0) circle [radius=0.05];
+            \fill[] (0,1) circle [radius=0.05];
+            \fill[] (\varT+180:1) circle [radius=0.05];
+            \fill[] (\varT:1) circle [radius=0.05];
+            \draw[red,|-Latex] ({tan(\varT)},-1.5) -- ({tan(\varT)},0);
+        \end{tikzpicture}
+    \end{frame}
 \end{document}
 '''
 
@@ -64,13 +50,13 @@ def main():
     for angle in np.linspace(-30,50,numiter//2):
         with open(animatetex.TeX_file, 'w') as f:
             f.write(start)
-            f.write(r'\newcommand{\mytheta}{' +f'{angle}' +'}')
+            f.write(r'\newcommand{\varT}{' +f'{angle}' +'}')
             f.write(end)
         animatetex.during_loop()
     for angle in np.linspace(50,-30,numiter//2):
         with open(animatetex.TeX_file, 'w') as f:
             f.write(start)
-            f.write(r'\newcommand{\mytheta}{' +f'{angle}' +'}')
+            f.write(r'\newcommand{\varT}{' +f'{angle}' +'}')
             f.write(end)
         animatetex.during_loop()
     animatetex.after_loop()
